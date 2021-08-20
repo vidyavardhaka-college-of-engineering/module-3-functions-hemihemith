9e90ad46c36c7d8ac9168275642094c3d6e980b6
hemihemith <84697170+hemihemith@users.noreply.github.com>
2021-08-20 19:41:07 +0530

    Update Program-10.c

--- Program-10.c
+++ Program-10.c
@@ -3,7 +3,7 @@
 	output: 3*/
 	#include<stdio.h>
 	//write the function prototype
-	{
+	int sub(int a,int b){
 	     int c=a-b;
 	     return c;
 	}
@@ -13,6 +13,6 @@
 	    printf("enter two numbers");
 	scanf("%d%d",&var1,&var2);
 	    var3=sub(var1,var2);
-	    printf("%d",);//complete the missing data
+	    printf("%d",var3);//complete the missing data
 	    return 0;
 	}
